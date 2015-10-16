a977d7a74603ee173fbd6cda46cf1e8534de8d7f
HafidTraikzi <hafid.traikzi@gmail.com>
2015-10-16 15:37:45 +0200

    Edit timer

--- quizzer.js
+++ quizzer.js
@@ -1,30 +1,19 @@
 Quizz = new Mongo.Collection("quizz");
 
-function timer() {
-    var interval = setInterval(function () {
-        var timer = $('span.timer').html();
-        timer = timer.split(':');
-        minutes = parseInt(timer[0], 10);
-        seconds = parseInt(timer[1], 10);
-        seconds -= 1;
-        if (minutes < 0)
-            return clearInterval(interval);
-        if (minutes < 10 && minutes.length != 2)
-            minutes = '0' + minutes;
-        if (seconds < 0 && minutes != 0) {
-            minutes -= 1;
-            seconds = 59;
-        }
-        else if (seconds < 10 && length.seconds != 2)
-            seconds = '0' + seconds;
-        $('span.timer').html(minutes + ':' + seconds);
+var count = 30;
 
-        if (minutes == 0 && seconds == 0) {
-            clearInterval(interval);
-  
-            Router.go('/quizz' + numero);
-        }
-    }, 1000);
+var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer() {
+    count = count - 1;
+    if (count <= 0)
+    {
+        clearInterval(counter);
+        Router.go('/quizz');
+        return;
+    }
+
+    document.getElementById("timer").innerHTML = count;
 }
 
 if (Meteor.isClient) {
@@ -35,71 +24,71 @@ if (Meteor.isClient) {
     });
     Router.route('/image');
     Router.route('/quizz');
-    
+
     Router.route('/erreur');
     Router.route('/gagne');
-    
+
     Router.route('/fin');
-    
+
     Template.image.rendered = function () {
         if (!this._rendered) {
             this._rendered = true;
         }
         timer();
     }
-    
+
     Template.image.helpers({
-       image1: function () {
-           if (numero == "1") {
+        image1: function () {
+            if (numero == "1") {
                 return true;
             }
-       },
-       image2: function () {
-           if (numero == "2") {
+        },
+        image2: function () {
+            if (numero == "2") {
                 return true;
             }
-       }, 
-       image3: function () {
-           if (numero == "3") {
+        },
+        image3: function () {
+            if (numero == "3") {
                 return true;
             }
-       }
+        }
     });
-    
+
     Template.erreur.helpers({
-       image1: function () {
-           if (numero == "1") {
+        image1: function () {
+            if (numero == "1") {
                 return true;
             }
-       },
-       image2: function () {
-           if (numero == "2") {
+        },
+        image2: function () {
+            if (numero == "2") {
                 return true;
             }
-       }, 
-       image3: function () {
-           if (numero == "3") {
+        },
+        image3: function () {
+            if (numero == "3") {
                 return true;
             }
-       }
+        }
     });
-    
+
     Template.gagne.helpers({
-       image1: function () {
-           if (numero == "1") {
+        image1: function () {
+            if (numero == "1") {
                 return true;
             }
-       },
-       image2: function () {
-           if (numero == "2") {
+        },
+        image2: function () {
+            if (numero == "2") {
                 return true;
             }
-       }, 
-       image3: function () {
-           if (numero == "3") {
+        },
+        image3: function () {
+            if (numero == "3") {
                 return true;
             }
-       }
+        }
     });
 }
 
